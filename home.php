8336a7d4e89d4379f0bd87aa2523993c31993a72
AshileySabah <ashileysabrina2002@gmail.com>
2021-05-24 22:13:12 -0300

    home edit

--- home.php
+++ home.php
@@ -43,7 +43,19 @@
 	  </div>
 	</nav>
   	<!-- FIM: NAVEGACAO TOPO -->
-
+  	<div align="center" style="margin: 50px 0;" class="display-4">
+  		00:00:00
+  	</div>
+  	<div class="container">
+	  <div class="row">
+	    <div class="col">
+	      <button style="width: 300px;" type="button" class="btn btn-primary">INICIAR</button>
+	    </div>
+	    <div class="col">
+	      Registros
+	    </div>
+	  </div>
+	</div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   </body>
