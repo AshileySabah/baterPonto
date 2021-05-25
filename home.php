bc52e84057112362278fbd0c34c14bfcda989a43
AshileySabah <ashileysabrina2002@gmail.com>
2021-05-25 20:59:40 -0300

    navegacao topo

--- home.php
+++ home.php
@@ -30,10 +30,10 @@
 	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 	      <span class="navbar-toggler-icon"></span>
 	    </button>
-	    <div id="navegacao" style="margin-left: 30px;" class="collapse navbar-collapse" id="navbarText">
+	    <div class="collapse navbar-collapse" id="navbarText">
 	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-	        <li class="nav-item">
-	          <a class="nav-link active" aria-current="page" href="relatorios.php">Relatórios</a>
+	        <li id="navegacao" class="nav-item">
+	          <a style="margin-left: 30px;" class="nav-link active" aria-current="page" href="relatorios.php">Relatórios</a>
 	        </li>
 	      </ul>
 	      <span class="navbar-text">
