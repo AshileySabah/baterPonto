c96ea6362e714b8f3c26d0b221733bd8508f1a33
AshileySabah <ashileysabrina2002@gmail.com>
2021-05-26 22:33:18 -0300

    registrar tabela

--- home.php
+++ home.php
@@ -5,6 +5,12 @@
 	if($_SESSION['login'] == false){
 		header('Location: index.php');
 	}
+
+	$tabela = 0;
+	$contarRegistro = 0;
+
+	$listaRegistros = [];
+	$indiceListaRegistros = 0;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -55,32 +61,61 @@
   			document.getElementById("mostrarRelogio").value = tempo;
   		}
 
-  		var temporizar = setInterval(relogio, 1000);
+  		var temporizar = setInterval(relogio, 50);
   	</script>
   	<div id="caixaRelogio" align="center" style="margin: 50px 0;">
   		<input style="text-align: center;" class="display-4" type="text" disabled id="mostrarRelogio">
   	</div>
   	<div class="container">
 	  <div class="row">
-	    <div class="col-sm" align="center">
-	      <button onclick="registrar()" style="width: 300px;" type="button" class="btn btn-primary mb-5">INICIAR</button>
-	    </div>
+	    <form method="post" class="col-sm" align="center">
+	      <button style="width: 300px;" name="btnRegistro" type="submit" class="btn btn-primary mb-5">INICIAR</button>
+	    </form>
 	    <div class="col-sm" align="left">
-	      Registros
+	      <h5>Registros</h5>
+	      <?php if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["btnRegistro"])){ 
+	      	$horarioRegistro = date('H:i:s');
+	      	$contarRegistro++;
+	      	$nomeInput = "registro$contarRegistro";
+
+	      	$listaRegistros[$indiceListaRegistros] = $horarioRegistro;
+	      	$indiceListaRegistros++;
+	      ?><!-- início SE POST E BOTÃO SETADO -->
+	      	<?php if($tabela == 0){
+	      		$tabela++;
+	      	?><!-- CONDIÇÃO FORMAR TABELA -->
+	      		<table class="table table-striped">
+	      			<tbody>
+						<tr>
+							<td>Horário</td>
+							<td>Descrição</td>
+						</tr>
+	      	<?php } ?><!-- CONDIÇÃO FORMAR TABELA -->
+	      	<?php if($tabela > 0){ ?><!-- DEMAIS REGISTROS -->
+	      		<?php foreach ($listaRegistros as $cadaRegistro){ ?>
+	      			<tr>
+						<td><?php echo $cadaRegistro ?></td>
+						<td>Descrição</td>
+					</tr>
+	      		<?php } ?>
+	      	<?php } ?><!-- DEMAIS REGISTROS -->
+	      <?php } ?><!-- fim SE POST E BOTÃO SETADO -->
 	    </div>
 	  </div>
 	</div>
-	<script>
-		function registrar(){
-			var horario = new Date();
-  			var hora = horario.getHours();
-  			var minuto = horario.getMinutes();
-  			var segundo = horario.getSeconds();
-
-  			var tempo = hora+":"+minuto+":"+segundo;
-  			document.write(tempo)
-		}
-	</script>
+	
+	<!--
+	<table border="1">
+		<tbody>
+			<tr>
+				<td>Registro</td>
+				<td>Descrição</td>
+			</tr>
+			<tr>
+				<td><?php echo $horarioRegistro ?></td>
+				<td><input type="text" disabled name="<?php echo $nomeInput ?>" value="Início"></td>
+			</tr>-->
+	
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   </body>
