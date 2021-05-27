ce4b749c3d96cc37bbc82eb54cb7352913c61ba0
AshileySabah <ashileysabrina2002@gmail.com>
2021-05-26 21:34:57 -0300

    registrar tempo com javascript botao

--- home.php
+++ home.php
@@ -63,13 +63,24 @@
   	<div class="container">
 	  <div class="row">
 	    <div class="col-sm" align="center">
-	      <button style="width: 300px;" type="button" class="btn btn-primary mb-5">INICIAR</button>
+	      <button onclick="registrar()" style="width: 300px;" type="button" class="btn btn-primary mb-5">INICIAR</button>
 	    </div>
 	    <div class="col-sm" align="left">
 	      Registros
 	    </div>
 	  </div>
 	</div>
+	<script>
+		function registrar(){
+			var horario = new Date();
+  			var hora = horario.getHours();
+  			var minuto = horario.getMinutes();
+  			var segundo = horario.getSeconds();
+
+  			var tempo = hora+":"+minuto+":"+segundo;
+  			document.write(tempo)
+		}
+	</script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   </body>
